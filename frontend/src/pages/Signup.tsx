29a17b6b9480c7b32c272c53ea19a03ad0210bcd
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-06-16 19:25:22 +0530

    frontend basic routes setup

---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,20 @@
+import { Quote } from '../components/quote';
+
+const Signup = () => {
+  return (
+    <div>
+        <div className="grid grid-cols-2">
+            <div>
+                hi
+            </div>
+            <div className='invisible md:visible'>
+            <Quote />
+            </div>
+            
+        </div>
+        
+    </div>
+  )
+}
+
+export default Signup
